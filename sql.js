ab769b5e02b0110e8b8abe0e0a5edcae884bfe0b
match19 <match19@student.sdu.dk>
2021-11-21 16:05:51 +0100

    end connection after use

--- sql.js
+++ sql.js
@@ -1,16 +1,17 @@
 const { Client } = require('pg')
-const client = new Client({
-    user: 'ttxwvksu',
-    host: 'abul.db.elephantsql.com',
-    database: 'ttxwvksu',
-    password: 'lB1FYYY7AfT-EO-k1ZK4maLDT_nxL522',
-    port: 5432,
-  })
-client.connect()
 
 module.exports.query = async function(query, param = null) {
     try {
+        const client = new Client({
+            user: 'ttxwvksu',
+            host: 'abul.db.elephantsql.com',
+            database: 'ttxwvksu',
+            password: 'lB1FYYY7AfT-EO-k1ZK4maLDT_nxL522',
+            port: 5432,
+          })
+        client.connect()
         const res = await client.query(query, param);
+        client.end()
         return res;
     } catch (error) {
         throw {statusCode: 500, msg: "Internal server error"};
